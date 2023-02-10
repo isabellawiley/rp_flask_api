df61d34b89ab73d950521b59c8ac3ad1c30d7117
Isabella <wileyisabella@gmail.com>
2023-02-10 03:34:06 -0500

    fixed error in _person_content.html file, everything works yay!

--- static/js/index.js
+++ static/js/index.js
@@ -3,12 +3,12 @@ import { Notes } from "./notes.js";
 import { DebugForm } from "./debug.js";
 
 function main() {
-    new People();
-    new Notes();
-    if (document.querySelector(".debug-card")) {
-        const debug = new DebugForm();
-        debug.showResponse("");
-    }
+  new People();
+  new Notes();
+  if (document.querySelector(".debug-card")) {
+    const debug = new DebugForm();
+    debug.showResponse("");
+  }
 }
 
 main();
